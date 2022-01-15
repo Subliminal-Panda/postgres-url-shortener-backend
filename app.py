19a2c82e2a07f53206f4664815c9fd0fcaab1920
Tristan Alexander Mouritsen <tmouritsen57@gmail.com>
2022-01-14 17:14:54 -0700

    updated readme file

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 CORS(app)
 
+# creates link class and
+
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     stored_url = db.Column(db.String, nullable=False)
@@ -24,12 +26,16 @@ class Link(db.Model):
         self.stored_url = stored_url
         self.stored_link = stored_link
 
+# sets up the schema for the database:
+
 class LinkSchema(ma.Schema):
     class Meta:
         fields = ('id', 'stored_url', 'stored_link')
 
 link_schema = LinkSchema()
 multiple_link_schema = LinkSchema(many=True)
+
+# route to add a new link:
 
 @app.route('/url/add', methods=['POST'])
 def add_link():
@@ -41,14 +47,24 @@ def add_link():
     stored_link = post_data.get("stored_link")
 
     if stored_link == "":
+
+        # generate random short link:
+
         saved_link = "".join([random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10)])
         possible_duplicate = db.session.query(Link).filter(Link.stored_link == saved_link).first()
+
+        # check for duplicates, re-generate random link until no duplicates are found:
 
         while possible_duplicate is not None:
             saved_link = "".join([random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10)])
             possible_duplicate = db.session.query(Link).filter(Link.stored_link == saved_link).first()
+
+    # saves user input as short link, if it exists:
+
     else:
         saved_link = stored_link
+
+    # stores new link/url data in a variable, and saves it in the database:
 
     new_link = Link(stored_url, saved_link)
 
@@ -58,15 +74,21 @@ def add_link():
     successful = ["New link added to database:", link_schema.dump(new_link)]
     return jsonify(successful)
 
+# route to retrieve all links:
+
 @app.route('/url/get', methods=["GET"])
 def get_all_links():
     all_links = db.session.query(Link).all()
     return jsonify(multiple_link_schema.dump(all_links))
 
+# route to retrieve one link:
+
 @app.route('/url/get/link/<link>', methods=["GET"])
 def get_link(link):
     found_link = db.session.query(Link).filter(Link.stored_link == link).first()
     return jsonify(link_schema.dump(found_link))
+
+# route to delete one link by its shortlink name:
 
 @app.route("/url/delete/<link>", methods=['DELETE'])
 def delete_link(link):
@@ -75,6 +97,8 @@ def delete_link(link):
     db.session.commit()
     successful = ["The following link has been deleted:", link_schema.dump(link)]
     return jsonify(successful)
+
+# route to delete one link by its unique id:
 
 @app.route("/url/delete/id/<id>", methods=['DELETE'])
 def delete_link_by_id(id):
